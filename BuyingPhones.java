34536df5c56ea236334c0b9508b9f865fb0e172c
Manpreet <Manpreet@DESKTOP-8VTP474>
2019-06-18 15:57:56 -0800

    Problem1

---
+++ BuyingPhones.java
@@ -0,0 +1,14 @@
+
+	import java.util.Scanner;
+
+	public class BuyingPhones {
+
+		public static void main(String[] args) {
+			System.out.println("Enter the regular price of a phone: ");
+			Scanner s = new Scanner(System.in);
+			double price = s.nextDouble();
+			double price3=price/2.0;
+		}
+		
+
+	}

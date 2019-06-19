6f9825a8c30e7e986c88a5f88bc56a1ac7eba37e
Manpreet <Manpreet@DESKTOP-8VTP474>
2019-06-18 17:10:46 -0800

    Problem 3 rq5

--- Main.java
+++ Main.java
@@ -1,13 +1,22 @@
 public class Main {	
 	public static void main(String[] args) throws InterruptedException {
-		Rabbit rabbit = new Rabbit(0,10);
+		Rabbit rabbit = new Rabbit(20,10);
 		rabbit.sayHello();
 		boolean runForever = true;
 
 		while (runForever == true) {
 			
 			System.out.println("Rabbit is at"+rabbit.printCurrentPosition());
+			if(rabbit.getxPosition()>0)
 			rabbit.moveLeft();
+			else
+			break;
+			Thread.sleep(1000);
+		}
+		while (runForever == true) {
+			
+			System.out.println("Rabbit is at"+rabbit.printCurrentPosition());
+			rabbit.moveRight();
 			Thread.sleep(1000);
 		}
 	}

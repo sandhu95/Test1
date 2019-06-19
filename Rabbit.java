f44f3be617666709a2d57e288aa4ba1745874a5a
Manpreet <Manpreet@DESKTOP-8VTP474>
2019-06-18 16:38:07 -0800

    Problem3 rq1

---
+++ Rabbit.java
@@ -0,0 +1,42 @@
+
+public class Rabbit {
+	
+	// -----------
+	// PROPERTIES
+	// -----------
+	private int xPosition=0;
+	private int yPosition=0;
+	
+	
+	// -----------
+	// CONSTRUCTOR 
+	// ------------
+	public Rabbit(int xPosition, int yPosition) 
+	{
+		this.xPosition=xPosition;
+		this.yPosition=yPosition;
+	}
+	
+	// -----------
+	// METHODS 
+	// ------------
+	public void printCurrentPosition() {
+		System.out.println("The current position of the rabbit is: ");
+	}
+
+	public void sayHello() {
+		System.out.println("Hello! I am a rabbit!");
+	}
+	
+	// ----------------
+	// ACCESSOR METHODS
+	// ----------------
+	
+	// Put all your accessor methods in this section.
+	
+	
+}
+
+
+
+
